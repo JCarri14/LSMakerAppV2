0fd23c4d2a6034407793b46c9d28b37ca844a6f6
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-12 20:07:28 +0200

    Update ScanViewModel.java

--- app/src/main/java/com/salle/projects/lsmakerappv2/viewmodel/ScanViewModel.java
+++ app/src/main/java/com/salle/projects/lsmakerappv2/viewmodel/ScanViewModel.java
@@ -6,22 +6,31 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.salle.projects.lsmakerappv2.model.Device;
+
 import java.util.List;
 import java.util.Map;
 
 public class ScanViewModel extends ViewModel {
 
-    private MutableLiveData<List<BluetoothDevice>> mDevices;
+    private MutableLiveData<List<BluetoothDevice>> mDevicesObj;
     private MutableLiveData<Map<String, Integer>> mRssiValues;
+
+    private MutableLiveData<List<Device>> mDevices;
 
     public void requestBluetoothScan() {}
 
-    public LiveData<List<BluetoothDevice>> getBluetoothDevices() {
-        return mDevices;
+    public LiveData<List<BluetoothDevice>> getBluetoothDevicesObj() {
+        return mDevicesObj;
     }
 
     public LiveData<Map<String, Integer>> getRssiValues() {
         return mRssiValues;
     }
+
+    public LiveData<List<Device>> getBluetoothDevices() {
+        return mDevices;
+    }
+
 
 }
