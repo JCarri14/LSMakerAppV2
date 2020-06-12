34663f479688de7c379580cafdcb20d1b7e23b28
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-12 19:16:32 +0200

    Create ConfigActivity.java

---
+++ app/src/main/java/com/salle/projects/lsmakerappv2/view/ui/ConfigActivity.java
@@ -0,0 +1,16 @@
+package com.salle.projects.lsmakerappv2.view.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfigActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
