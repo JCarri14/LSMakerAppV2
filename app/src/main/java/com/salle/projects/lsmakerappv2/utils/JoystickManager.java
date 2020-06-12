23ac7399ae31d71da53aee7a98e07fe09b94e694
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-12 19:16:37 +0200

    Create JoystickManager.java

---
+++ app/src/main/java/com/salle/projects/lsmakerappv2/utils/JoystickManager.java
@@ -0,0 +1,8 @@
+package com.salle.projects.lsmakerappv2.utils;
+
+public class JoystickManager {
+
+    public static int[] getDirectionFromParams(int angle, int strength) {
+        return new int[0];
+    }
+}
