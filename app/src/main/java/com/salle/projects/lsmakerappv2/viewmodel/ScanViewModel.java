8072f064b5dd628b7e6f4f9e805a1fb149eb7e21
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-13 21:30:10 +0200

    Update ScanViewModel.java

--- app/src/main/java/com/salle/projects/lsmakerappv2/viewmodel/ScanViewModel.java
+++ app/src/main/java/com/salle/projects/lsmakerappv2/viewmodel/ScanViewModel.java
@@ -6,31 +6,49 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.salle.projects.lsmakerappv2.model.Device;
+import com.salle.projects.lsmakerappv2.bluetooth.BluetoothService;
+import com.salle.projects.lsmakerappv2.bluetooth.callbacks.BtDiscoveryCallback;
+import com.salle.projects.lsmakerappv2.model.BtDevice;
+import com.salle.projects.lsmakerappv2.utils.BluetoothDeviceComparator;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
-public class ScanViewModel extends ViewModel {
+public class ScanViewModel extends ViewModel implements BtDiscoveryCallback {
 
-    private MutableLiveData<List<BluetoothDevice>> mDevicesObj;
-    private MutableLiveData<Map<String, Integer>> mRssiValues;
-
-    private MutableLiveData<List<Device>> mDevices;
+    private MutableLiveData<List<BtDevice>> mDevices;
 
     public void requestBluetoothScan() {}
 
-    public LiveData<List<BluetoothDevice>> getBluetoothDevicesObj() {
-        return mDevicesObj;
-    }
-
-    public LiveData<Map<String, Integer>> getRssiValues() {
-        return mRssiValues;
-    }
-
-    public LiveData<List<Device>> getBluetoothDevices() {
+    public LiveData<List<BtDevice>> getBluetoothDevices() {
         return mDevices;
     }
 
+    /*****************************************************************************
+     * ***************************  SCANNING CALLBACK  ***************************/
+    @Override
+    public void onDeviceDiscovered(BluetoothDevice device, int rssi) {
+        boolean deviceFound = false;
+        if (mDevices.getValue() != null) {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                deviceFound = mDevices.getValue().stream()
+                        .anyMatch(d -> d.getAddress().equals(device.getAddress()));
+            } else {
+                for (BtDevice listDev : mDevices.getValue()) {
+                    if (listDev.getAddress().equals(device.getAddress())) {
+                        deviceFound = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if (!deviceFound) {
+            if (mDevices.getValue() == null) { mDevices.setValue(new ArrayList<>());}
+            mDevices.getValue().add(new BtDevice(device.getName(), device.getAddress(), rssi));
+            Collections.sort(mDevices.getValue(), new BluetoothDeviceComparator());
+        }
+
+    }
 
 }
