a60d0e09942bc72c01dfb1357c97817dbe5847d6
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-15 16:14:00 +0200

    Update ConfigActivity.java

--- app/src/main/java/com/salle/projects/lsmakerappv2/view/ui/ConfigActivity.java
+++ app/src/main/java/com/salle/projects/lsmakerappv2/view/ui/ConfigActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.salle.projects.lsmakerappv2.R;
+
 public class ConfigActivity extends AppCompatActivity {
 
 
@@ -12,5 +14,9 @@ public class ConfigActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+        initViews();
     }
+
+    private void initViews() {}
 }
