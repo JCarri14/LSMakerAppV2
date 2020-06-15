89e93b77553db909585f8ba20bd2ef6892134ce4
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-15 21:35:44 +0200

    Update MainActivity.java

--- app/src/main/java/com/salle/projects/lsmakerappv2/view/ui/MainActivity.java
+++ app/src/main/java/com/salle/projects/lsmakerappv2/view/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.salle.projects.lsmakerappv2.view.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,19 +10,28 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.salle.projects.lsmakerappv2.R;
+import com.salle.projects.lsmakerappv2.bluetooth.BluetoothService;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
     private Button btnConnect, btnDrive,btnConfig;
+    private TextView tvState, tvName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkDeviceInformation();
     }
 
     private void initViews() {
@@ -50,5 +61,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        tvState = findViewById(R.id.activity_main_state);
+        tvName = findViewById(R.id.activity_main_device);
+        checkDeviceInformation();
+    }
+
+    private void checkDeviceInformation() {
+        BluetoothService manager = BluetoothService.getInstance();
+        if (manager.getDevice() != null) {
+            tvState.setText(getText(R.string.connection_state_connected));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                tvState.setTextColor(getColor(R.color.colorConnected));
+            } else {
+                tvState.setTextColor(ContextCompat.getColor(this, R.color.colorConnected));
+            }
+            tvName.setText(manager.getDevice().getName());
+        }
     }
 }
