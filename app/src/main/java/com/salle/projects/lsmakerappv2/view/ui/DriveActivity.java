14c7955e956cf0156a899aa51cfad525fcf695d5
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-15 18:27:45 +0200

    Update DriveActivity.java

--- app/src/main/java/com/salle/projects/lsmakerappv2/view/ui/DriveActivity.java
+++ app/src/main/java/com/salle/projects/lsmakerappv2/view/ui/DriveActivity.java
@@ -1,14 +1,26 @@
 package com.salle.projects.lsmakerappv2.view.ui;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.salle.projects.lsmakerappv2.R;
-import com.salle.projects.lsmakerappv2.utils.JoystickManager;
+import com.salle.projects.lsmakerappv2.services.TiltService;
+import com.salle.projects.lsmakerappv2.utils.JoystickUtils;
 import com.salle.projects.lsmakerappv2.view.managers.DrivingDataManager;
 import com.warkiz.widget.IndicatorSeekBar;
 import com.warkiz.widget.OnSeekChangeListener;
@@ -21,11 +33,31 @@ public class DriveActivity extends AppCompatActivity {
     // UI attributes
     private Button btnBack, btnMode;
     private Button btnSlowest, btnSlow, btnNormal, btnFast;
+    private ImageView ivIcon;
     private JoystickView jvControl;
     private IndicatorSeekBar iVelocity;
 
     // Data Manager
     private DrivingDataManager mManager;
+
+    // Dialog mode options
+    private CharSequence[] mModeItems = new CharSequence[]{DrivingDataManager.JOYSTICK_MODE,
+            DrivingDataManager.ROTATION_MODE};
+
+    /** Broadcast receiver to listen to the TiltService's changes.*/
+    private BroadcastReceiver tiltReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            //*********************//
+            if (action.equals(TiltService.TILT_DATA_UPDATED)) {
+                //currentSpeedAngle = TiltService.getRoll();
+                if (mManager == null) { mManager = DrivingDataManager.getInstance();}
+                mManager.setTurn(getTurn(TiltService.getPitch()));
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,6 +65,25 @@ public class DriveActivity extends AppCompatActivity {
         setContentView(R.layout.activity_driving);
         mManager = DrivingDataManager.getInstance();
         initViews();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mManager != null && mManager.getDataSource().equals(DrivingDataManager.ROTATION_MODE)) {
+            TiltService.stopService();
+            this.unregisterReceiver(tiltReceiver);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mManager != null && mManager.getDataSource().equals(DrivingDataManager.ROTATION_MODE)) {
+            TiltService.initializeService(this);
+            this.registerReceiver(tiltReceiver, intentFilter);
+        }
     }
 
     private void initViews() {
@@ -56,7 +107,7 @@ public class DriveActivity extends AppCompatActivity {
         jvControl.setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
             public void onMove(int angle, int strength) {
-                int turn = JoystickManager.getDirectionFromParams(angle, strength);
+                int turn = JoystickUtils.getDirectionFromParams(angle, strength);
                 mManager.setTurn(turn);
             }
         });
@@ -110,4 +161,79 @@ public class DriveActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    /**
+     * Method that returns the current turn set by the smartphone's orientation.
+     *
+     * The current turn can be calculated using the initial turn (considered 0º or horizontal
+     * position) and the last turn angle gotten from the Tilt service. Maximum turn will be
+     * achieved on a tilt >= 45º.
+     *
+     * @return the current turn set by the smartphone's orientation
+     */
+    private int getTurn(Float turnAngle) {
+        if (mManager.isRunning()) {
+            float angle = turnAngle;
+            if (angle > 45) angle = 45;
+            if (angle < -45) angle = -45;
+            return Math.round(angle * 100 / 45);
+        }
+        return 0;
+    }
+
+    /*****************************************************************************
+     * ***************************  MODES DIALOG  ******************************/
+    private void displayModeDialog() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setTitle("Choose the Driving Mode");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder.setIcon(getResources().getDrawable(R.drawable.ic_control, null));
+        }
+        //.setBackground(getResources().getDrawable(R.drawable.back_white_rad, null))
+        builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setSingleChoiceItems(mModeItems, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.show();
+    }
+
+    @SuppressLint("SourceLockedOrientationActivity")
+    private void setupOnDeviceRotationMode() {
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation != Configuration.ORIENTATION_LANDSCAPE) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        // Turn visible the device icon
+        ivIcon = findViewById(R.id.activity_driving_icon);
+        ivIcon.setVisibility(View.VISIBLE);
+
+        // Make invisible the joystick command
+        jvControl.setVisibility(View.GONE);
+
+        // Setting the current control mode
+        mManager.setDataSource(DrivingDataManager.ROTATION_MODE);
+
+        // Accelerometer
+        TiltService.initializeService(this);
+        this.registerReceiver(tiltReceiver, intentFilter);
+    }
+
+    /** IntentFilter to configure the broadcast receiver */
+    private IntentFilter intentFilter = new IntentFilter(TiltService.TILT_DATA_UPDATED);
+
 }
